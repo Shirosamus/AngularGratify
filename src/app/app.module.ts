3d14d5f6855c94bb55f8e30787dc26db150f38ae
Unknown <hylian.mesmoudi@etu.umontpellier.fr>
2018-11-27 11:39:48 +0100

    ng2-file-upload installed
    npm install ng2-file-upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FileSelectDirective } from 'ng2-file-upload';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { UploadFormComponent } from './upload-form/upload-form.component';
 @NgModule({
   declarations: [
     AppComponent,
+    FileSelectDirective,
     MusicListViewComponent,
     HomeViewComponent,
     MusicComponent,
